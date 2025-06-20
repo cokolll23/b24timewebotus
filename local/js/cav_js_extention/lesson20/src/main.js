f9a15a31b477a305d29d2671e4fdf8089035597f
ALEX <cavjob@ya.ru>
2025-06-20 14:22:37 +0300

    20lesson дз

--- local/js/cav_js_extention/lesson20/src/main.js
+++ local/js/cav_js_extention/lesson20/src/main.js
@@ -2,11 +2,26 @@ BX.namespace('Cav_js_extention.Lesson20');
 
 BX.Cav_js_extention.Lesson20 = {
     showMessage: function (message = '') {
-       // alert('ok');
+        // alert('ok');
     },
     onStartWorkingDateAction: function (popupNodeId) {
+        const objTimemanMain = BX('timeman_main');
+        let objUiBtnTd = BX.findChild(objTimemanMain, {tag: 'td', className: 'tm-popup-timeman-layout-button'}, true);// tm-popup-timeman-layout-button
+        let objUiBtnSuccess = BX.findChild(objUiBtnTd, {tag: 'button', className: 'ui-btn'}, true);// tm-popup-timeman-layout-button
+        let bUiBtnSuccessHasClass = BX.hasClass(objUiBtnSuccess, 'ui-btn-icon-start');
+        //alert(bUiBtnSuccessHasClass);
+
+        if (bUiBtnSuccessHasClass == true) {
+            var strTextTitle = 'Вы хотите начать рабочий день?';
+            var strPopupWindowButton = 'Начать рабочий день';
+            var strPopupWindowButtonClass = 'ui-btn-success PopupWindowButtonStart';
+        } else {
+            strTextTitle = 'Вы хотите закончить рабочий день?';
+            strPopupWindowButton = 'Закончить рабочий день';
+            strPopupWindowButtonClass = 'ui-btn-danger PopupWindowButtonStop';//ui-btn-danger
+        }
         var popup = BX.PopupWindowManager.create("greeting-popup-message", BX(popupNodeId), {
-            content: 'Вы хотите начать рабочий день?',
+            content: strTextTitle,
             width: 600, // ширина окна
             height: 400, // высота окна
             zIndex: 1100, // z-index
@@ -32,13 +47,20 @@ BX.Cav_js_extention.Lesson20 = {
             },
             buttons: [
                 new BX.PopupWindowButton({
-                    text: 'Начать рабочий день', // текст кнопки
+                    text: strPopupWindowButton, // текст кнопки
                     id: 'save-btn', // идентификатор
-                    className: 'ui-btn ui-btn-success', // доп. классы
+                    className: 'ui-btn  ' + ' ' + strPopupWindowButtonClass, // доп. классы
                     events: {
-                        click: function() {
-                            BX.Cav_js_extention.Lesson20.startDate();
-                            popup.close();
+                        click: function () {
+                            if (bUiBtnSuccessHasClass) {
+                                BX.Cav_js_extention.Lesson20.startDate();
+                                popup.destroy();
+                                popup.close();
+                            } else {
+                                BX.Cav_js_extention.Lesson20.endDate();
+                                popup.destroy();
+                                popup.close();
+                            }
                         }
                     }
                 }),
@@ -47,17 +69,17 @@ BX.Cav_js_extention.Lesson20 = {
                     id: 'copy-btn',
                     className: 'ui-btn ui-btn-primary',
                     events: {
-                        click: function() {
+                        click: function () {
                             popup.close();
                         }
                     }
                 })
             ],
             events: {
-                onPopupShow: function() {
+                onPopupShow: function () {
                     BX.Cav_js_extention.Lesson20.showMessage('Вы открыли окно начала рабочего дня!');
                 },
-                onPopupClose: function() {
+                onPopupClose: function () {
                     BX.Cav_js_extention.Lesson20.showMessage('Вы закрыли окно начала рабочего дня!');
                 }
             }
@@ -67,44 +89,16 @@ BX.Cav_js_extention.Lesson20 = {
     },
     startDate: function (e) {
         $('#timeman_main .tm-popup-timeman-layout-button button.ui-btn.ui-btn-icon-start').click();
+    },
+    endDate: function (e) {
+        $('#timeman_main .tm-popup-timeman-layout-button button.ui-btn.ui-btn-icon-stop').click();
     }
-
 };
-
 BX.addCustomEvent('onTimeManWindowBuild', function (e) {
+    // #timeman_main button ui-btn ui-btn-success     ui-btn-icon-start
+    BX.Cav_js_extention.Lesson20.onStartWorkingDateAction()
+});
+BX.addCustomEvent('onTimeManWindowOpen', function (e) {
     // #timeman_main
     BX.Cav_js_extention.Lesson20.onStartWorkingDateAction()
-    let startdayParentBlock =  BX('timeman_main ');
-    let startdayButton = document.querySelector('button.ui-btn.ui-btn-icon-start');
-   // todo отправка time date userId на handler и CTimeMan старт раб дня
-
-    console.log(e);
 });
-
-
-/*$('body').on('click', '#greeting-popup-message #save-btn', function (e) {
-
-    alert('ok');
-});*/
-
-// /bitrix/tools/timeman.php?action=check_module&site_id=s1&sessid=c295c74ff07b0b9f604eeb4c435851ed
-function sendAjax(url, data = {}, node_target = '') {
-    $.ajax({
-        type: method,
-        url: url,
-        data: data,
-        dataType: 'json',
-        cache: false,
-        success: function (data) {
-            if (data.success) {
-                let strSuccess = ` Уважаемый ${data.fio} , вы записались на ${data.date} на процедуру ${data.proceduraName} `;
-                $(' #popup-window-content-ajaxPopup ').html(strSuccess);
-                $('#ajaxPopup').find('#save-btn').css('display','none');
-                // alert(strSuccess);
-            }
-            //$( ' #popup-window-content-ajaxPopup form#popup').html(strSuccess);
-        }
-    })
-
-
-}
